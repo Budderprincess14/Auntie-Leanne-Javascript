47da51783a481b918c92b1505d68cc6d87e55191
Amaya Kennedy <43918092+Budderprincess14@users.noreply.github.com>
2020-06-06 13:43:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 var birthDay = "9";
 var birthMonth = "June";
 
-function createParagraph() {
-  let para = document.createElement('p');
-  para.textContent = 'You clicked the button!';
-  document.body.appendChild(para);
-}
-
-const buttons = document.querySelectorAll('button');
-
-for(let i = 0; i < buttons.length ; i++) {
-  buttons[i].addEventListener('click', birthdayMessage);
-}
-
 function birthdayMessage() {
-  var currentMonth = prompt("Please enter this month as a name.", "April");
+  var currentMonth = window.prompt("Please enter this month as a name.", "April");
   
   if(currentMonth == birthMonth) {
     currentDay();
   } else {
     return "Please come back in June!";
   }
+  
+  function currentDay() {
+    var todayDay = Number(window.prompt("Please enter today's date as a number."));
+    if(todayDay == birthDay) {
+      return "Please go to " + string.link("https://budderprincess14.github.io/Auntie-Leannes-Birthday-Card/") + " for your gift!";
+      break;
+    } else if (todayDay < birthDay) {
+      var remaining = birth - day;
+      return "Come back in " + remaining + " days!"
+      break;
+    } else {
+      return "Please come back next year!";
+    }
+  }
 }
 
+
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', birthdayMessage);
+}
