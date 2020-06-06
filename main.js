e925586b7c3393b495f4ba80d04b199ae3af4e18
Amaya Kennedy <43918092+Budderprincess14@users.noreply.github.com>
2020-06-06 13:12:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+var birthDay = "9";
+var birthMonth = "June";
+
+function birthdayMessage() {
+  var currentMonth = prompt("Please enter this month as a name.", "April");
+  
+  if(currentMonth == birthMonth) {
+    currentDay();
+  } else {
+    return "Please come back in June!";
+  }
+}
 
