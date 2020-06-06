4b26a63d0d11d5d16465c33d2929c1072d242ce7
Amaya Kennedy <43918092+Budderprincess14@users.noreply.github.com>
2020-06-06 13:25:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 var birthDay = "9";
 var birthMonth = "June";
 
+function createParagraph() {
+  let para = document.createElement('p');
+  para.textContent = 'You clicked the button!';
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', birthdayMessage);
+}
+
 function birthdayMessage() {
   var currentMonth = prompt("Please enter this month as a name.", "April");
   
